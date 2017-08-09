09bfe49984a13383962559063edfe473341b5160
greenteamuimui <siuding0601@gmail.com>
2017-08-08 20:11:33 -0700

    Refactor http get method to be a promise

--- server/api/routes.js
+++ server/api/routes.js
@@ -4,9 +4,12 @@ import { createQuery, apiCall } from './utils/suggestRecipe';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import User from './models/user';
+import http from 'http';
 
 const router = Router();
 const secret = '7x0jhxt&quot;9(thpX6';
+const app_id = "744f7b77";
+const app_key = "28d95b6af2869f1fdd36dcc5a7e6a24b";
 // delete unless needed
 
 router.get('/protected', (req, res, next) => {
@@ -29,8 +32,38 @@ router.route('/login').post((req, res, next) => login(req, res, next));
 
 router.route('/register').post((req, res) => register(req, res));
 
-router.get('/test', function(req, res){
-  apiCall(createQuery());
+router.get('/recipes', (req, res, next) => {
+  // passport.authenticate('jwt', (err, user, info) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   if (!user) {
+  //     return res.status(401).json({ error: 'Invalid credentials' });
+  //   }
+  //   if (user) {
+      let recipes = apiCall(createQuery()).then();
+      console.log("hi");
+      console.log(recipes);
+      return res
+        .status(200)
+        .json({recipes});
+    // }
+  // })(req, res, next);
+});
+
+router.get('/recipestest', (req, res, next) => {
+  let query = createQuery();
+  http.get(`http://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}`, (response) => {
+    response.setEncoding('utf8');
+    let rawData = '';
+    response.on('data', (chunk) => { rawData += chunk; });
+    response.on('end', () => {
+        const parsedData = JSON.parse(rawData);
+        let recipeinfo = parsedData.hits;
+        console.log(parsedData.hits);
+        res.status(200).json(recipeinfo);
+    });
+  });
 });
 
 // router.route('/users.json').get(userIndex);
--- server/api/utils/suggestRecipe.js
+++ server/api/utils/suggestRecipe.js
@@ -35,39 +35,37 @@ export const createQuery = () => {
 };
 
 export const apiCall = (query) => {
-  return (
-    http.get(`http://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}`, (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
+  http.get(`http://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}`, (res) => {
+    // const { statusCode } = res;
+    // const contentType = res.headers['content-type'];
+    //
+    // let error;
+    // if (statusCode !== 200) {
+    //   error = new Error('Request Failed.\n' +
+    //                     `Status Code: ${statusCode}`);
+    // } else if (!/^application\/json/.test(contentType)) {
+    //   error = new Error('Invalid content-type.\n' +
+    //                     `Expected application/json but received ${contentType}`);
+    // }
+    // if (error) {
+    //   console.error(error.message);
+    //   // consume response data to free up memory
+    //   res.resume();
+    //   return;
+    // }
 
-      let error;
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n' +
-                          `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('Invalid content-type.\n' +
-                          `Expected application/json but received ${contentType}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // consume response data to free up memory
-        res.resume();
-        return;
-      }
-
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          console.log(parsedData.hits);
-        } catch (e) {
-          console.error(e.message);
-        }
-      });
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    })
-  );
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      // try {
+        const parsedData = JSON.parse(rawData);
+        let recipeinfo = parsedData.hits;
+        // console.log(parsedData.hits);
+        return parsedData.hits;
+      // } catch (e) {
+        // console.error(e.message);
+      // }
+    });
+  });
 };
