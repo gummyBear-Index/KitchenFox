6794d7f68222b2c633499ff93264cdd8ada109d7
ky1007 <kevinXyang@outlook.com>
2017-08-10 19:26:02 -0700

    Revise styling

--- client/KitchenFox/style/layout.js
+++ client/KitchenFox/style/layout.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 
-// export const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     flexDirection: 'column'
-//   },
-//   button: {
-//     borderRadius: 4,
-//     padding: 20,
-//     marginBottom: 20,
-//     color: '#fff'
-//   },
-//   greenButton: {
-//     backgroundColor: '#4CD964'
-//   },
-//   centering: {
-//     justifyContent: 'center'
-//   }
-// });
-
 export const session = StyleSheet.create({
   container: {
     flex: 1,
     width: undefined,
     height: undefined,
   },
-  // darkenContainer: {
-  //   backgroundColor: 'rgba(0,0,0,.6)',
-  // },
   content: {
     paddingLeft: 20,
     paddingRight: 20,
