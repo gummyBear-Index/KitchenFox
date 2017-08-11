2e49f4bae9c2fd7bd080016d71de90796926b1d3
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-11 16:36:45 -0700

    add add_items_container and add_items

--- client/KitchenFox/components/pantry/add_items.js
+++ client/KitchenFox/components/pantry/add_items.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Container, Content, Text, View } from 'native-base';
-import crypto from 'crypto';
+import md5 from 'md5';
 
 import AddItemCard from './add_item_card';
 
@@ -9,15 +9,60 @@ class PantryAddItems extends React.Component {
   constructor() {
     super(props);
     this.state = {
-
+      cart: [],
+      numItemCards: 1,
     }
-
   }
 
-  genSku(name) {
-    const sku = crypto.createHash('md5').update(name).digest("hex");
-    return sku;
+  handleSubmit() {
+    let inventory = Object.assign(this.props.inventory);
+    this.cart.forEach((item) => {
+      let upc = item.upc.length ? item.upc : md5(item.name)
+      if (upc in inventory) {
+        inventory[upc].quantity += item.quantity;
+      } else {
+        inventory[upc] = {
+          name: item.name,
+          quantity: item.quantity,
+          units: item.units,
+        }
+        inventory[upc].weight = item.weight.length ? item.weight : 'NA';
+      }
+    });
+    this.props.sendItems(this.props.session.token, inventory);
+  };
+
+  handleCardUpdate(idx, childState) {
+    let obj = Object.assign(childState);
+    const newCart = this.state.cart.slice(0);
+    newCart[idx] = obj;
+    this.setState({ cart: newCart });
+    let numBlank = 0;
+    let blanks = newCart.filter(item => (
+      !(item.upc.length || item.name.length || item.name.quantity)
+    ));
+    blanks.length() === 0 ? updateNumItemCards(this.state.numItemCards + 1) : 0;
   }
+
+  updateNumItemCards(newNumItemCards) {
+    this.setState({ numItemCards: newNumItemCards });
+    this.itemFormGen(newNumItemCards);
+  }
+
+  itemFormGen(numItemCards) {
+    const itemCards = [];
+    for (let i = 0; i < numItemCards; i++) {
+      let j = (
+        <AddItemCard
+          key={`card-form-number-${i}`}
+          cardNum={i}
+          updateParent={(num, slice) => this.handleCardUpdate(num, slice)} />
+      );
+      itemCards.push(j);
+    }
+    return itemCards;
+  }
+
 
 
   static navigationOptions = {
@@ -27,9 +72,10 @@ class PantryAddItems extends React.Component {
 
 
   render() {
+    const itemCards = this.perkFormGen(this.state.numItemCards);
     return (
       <View>
-        <Text>ADD ITEMS HERE</Text>
+        {itemCards}
       </View>
     );
   }
