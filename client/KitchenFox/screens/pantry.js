44a5727c6e3f415082d3cba459b42c6abff3de30
Hiro Obara <hobara1211@gmail.com>
2017-08-12 13:32:55 -0700

    Added delete button that update qty to zero

--- client/KitchenFox/screens/pantry.js
+++ client/KitchenFox/screens/pantry.js
@@ -1,13 +1,13 @@
 import { StackNavigator } from 'react-navigation';
 import AddItemsContainer from '../components/pantry/add_items_container';
 import DashboardPantryStocked from '../components/dashboard/pantry_stocked';
-import PantryIndexContainer from '../components/pantry/pantry_index_container';
-import PantryItemContainer from '../components/pantry/pantry_item_container';
+import PantryIndex from '../components/pantry/pantry_index';
+import PantryItem from '../components/pantry/pantry_item';
 import RecipesIndex from '../components/recipes/recipes_index';
 
 const Pantry = StackNavigator({
-  PantryIndex: { screen: PantryIndexContainer },
-  PantryItem: { screen: PantryItemContainer },
+  PantryIndex: { screen: PantryIndex },
+  PantryItem: { screen: PantryItem },
   AddItem: { screen: AddItemsContainer },
   Dashboard: { screen: DashboardPantryStocked },
   Recipes: { screen: RecipesIndex },
