3546d20bbafaacbd88749db827e5f29836f78d9c
greenteamuimui <siuding0601@gmail.com>
2017-08-12 21:31:16 -0700

    Implement logic to add name to nested query object

--- client/KitchenFox/components/recipes/recipes_index.js
+++ client/KitchenFox/components/recipes/recipes_index.js
@@ -5,13 +5,16 @@ import { Container, Content, Header, View, DeckSwiper, Card, CardItem, Thumbnail
 import  {button} from '../../style/button';
 import { getRecipes } from '../../util/api_util';
 import  RecipeCard from './recipe_card';
+import CheckBox from 'react-native-checkbox';
+
 
 
 class RecipesIndex extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      recipes: []
+      recipes: [],
+      query: {},
     };
   };
   static navigationOptions = {
@@ -19,9 +22,38 @@ class RecipesIndex extends React.Component {
   };
 
   componentWillMount() {
+    this.props.requestItems(this.props.session.token);
     getRecipes(5, this.props.session.token).then((res) => {
       this.setState({recipes: JSON.parse(res._bodyText)})
     });
+  }
+
+  renderItems() {
+    const allId = Object.keys(this.props.inventory);
+    const allItems = [];
+    allId.forEach((id) => {
+      let obj = {};
+      obj[`${id}`] = this.props.inventory[`${id}`];
+      allItems.push(obj);
+    });
+
+    if (allItems.length > 0) {
+      return (
+        <View>
+        {allItems.map((item, idx) =>
+          <View style={pantry.itemContainer}>
+            <Text style={pantryText.item}>{Object.values(item)[0]['name']}</Text>
+            <Text style={pantryText.itemDesc}>{Object.values(item)[0]['quantity']}&nbsp;{Object.values(item)[0]['units']}</Text>
+              <CheckBox
+                label='Label'
+                checked={true}
+                onChange={(checked) => console.log('I am checked', checked)}
+              />
+          </View>
+        )}
+      </View>
+      );
+    }
   }
 
   recipes(){
