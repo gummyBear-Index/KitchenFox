7e0751497a64641735ef850607b9dfb7d56d9715
ky1007 <kevinXyang@outlook.com>
2017-08-12 19:57:33 -0700

    Restyle footer nav to blue color

--- client/KitchenFox/components/nav/footer.js
+++ client/KitchenFox/components/nav/footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
 import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
+import { BLUE, BLUE_DARK } from '../../style/common';
 
 // import PantryAddItems from '../pantry/add_items';
 // import DashboardPantryStocked from '../dashboard/pantry_stocked';
@@ -9,29 +10,33 @@ import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
 
 const NavFooter = ({ navigate }) => {
   // const { navigate } = this.props;
-
   return (
     <Footer>
-      <FooterTab>
+      <FooterTab style={{ backgroundColor: BLUE}}>
         <Button vertical
           onPress={() => navigate('AddItem')}>
-          <Icon name="add-circle" />
-          <Text>Add Item</Text>
+          <Icon name="add-circle" 
+                style={{ color: 'white'}} />
+          <Text style={{ color: 'white'}}>Add Item</Text>
         </Button>
         <Button vertical
           onPress={() => navigate('Dashboard')}>
-          <Icon name="clipboard" />
-          <Text>Dashboard</Text>
+          <Icon name="clipboard"
+                style={{ color: 'white'}} />
+          <Text style={{ color: 'white'}}>Dashboard</Text>
         </Button>
         <Button vertical
+          style={{ backgroundColor: BLUE_DARK }}
           onPress={() => navigate('Pantry')} active>
-          <Icon active name="albums" />
-          <Text>Pantry</Text>
+          <Icon active name="albums"
+                style={{ color: 'white'}} />
+          <Text style={{ color: 'white'}}>Pantry</Text>
         </Button>
         <Button vertical
           onPress={() => navigate('Recipes')}>
-          <Icon name="book" />
-          <Text>Recipes</Text>
+          <Icon name="book"
+                style={{ color: 'white'}} />
+          <Text style={{ color: 'white'}}>Recipes</Text>
         </Button>
       </FooterTab>
     </Footer>
